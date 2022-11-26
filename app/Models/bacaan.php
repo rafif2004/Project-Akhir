874de5a798af60018a445a729ae6540e60873051
rafif2004 <Rafif.aja14@gmail.com>
2022-11-26 13:45:39 +0700

    Sabtu

--- app/Models/bacaan.php
+++ app/Models/bacaan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class bacaan extends Model
 {
-    use HasFactory;
+    use HasFactory;  
     protected $guarded = ['id'];
     
     public function siswa()
